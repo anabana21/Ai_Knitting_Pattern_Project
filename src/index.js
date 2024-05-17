801cd907bca7b300c44325d29bbedd161fdf9dac
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-05-17 14:22:06 +0200

    Added JS and coreccted HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePattern(event) {
-  event.preventDefault();
-
+function displayPattern(response) {
   new Typewriter("#pattern", {
-    strings: "heart, stars and a flower",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePattern(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "fab7o44152df89e90853f216t75f2b9a";
+  let context =
+    "You are a knitting pattern expert and give the pattern written or as a visual help in knitting pattern font. Your mission is to generate a easy pattern in basic HTML. Make sure to follow the user instructions. ";
+  let prompt = `User instructions: Generate a knitting pattern about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPattern);
 }
 
 let patternFormElement = document.querySelector("#pattern-generator-form");
