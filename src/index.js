dff71797f1221e31990325c4fe3864632c8925a0
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-05-23 15:20:05 +0200

    AIinstructions adapted

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ function generatePattern(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "fab7o44152df89e90853f216t75f2b9a";
   let context =
-    "You are a knitting pattern software and specify the pattern in writing or as a visual aid in knitting pattern font. Your task is to create a simple pattern that is clear and easy to follow. The instructions should be reproduced in clear sections. The legend for the abbreviations should also be included. Make sure that you follow the instructions. ";
+    "You are a knitting pattern software and specify the pattern in writing or as a visual aid in knitting pattern font. Your task is to create a simple and short as possible pattern that is clear and easy to follow. If possible make to splits for the output. The legend for the abbreviations should also be included. Make sure that you follow the instructions. ";
   let prompt = `User instructions: Generate a knitting pattern about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let patternElement = document.querySelector("#pattern");
   patternElement.classList.remove("hidden");
-  patternElement.innerHTML = `<div class= "generating"> 🧶Generating a Knitting Pattern about ${instructionsInput.value}</div>`;
+  patternElement.innerHTML = `<div class="generating"> 🧶Generating a Knitting Pattern about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPattern);
 }
